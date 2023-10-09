001fcc0edbc61bd55f50d011ca0e0921b09f2aa1
Gokulraj V <gokulrajv123@gmail.com>
2023-10-09 15:13:14 +0530

    Add files via upload

---
+++ bookStore/src/main/java/com/bookStore/repository/MyBookList.java
@@ -0,0 +1,5 @@
+package com.bookStore.repository;
+
+public class MyBookList {
+
+}
---
+++ bookStore/src/main/java/com/bookStore/service/BookService.java
@@ -0,0 +1,29 @@
+package com.bookStore.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookStore.entity.Book;
+import com.bookStore.repository.BookRepository;
+
+@Service
+public class BookService {
+
+	@Autowired
+	private static BookRepository bRepo;
+	
+	public void save(Book b) {
+		bRepo.save(b);
+	}
+	public List<Book> getAllBook() {
+		return bRepo.findAll();
+	}
+	public Book getBookById(int id){
+		return bRepo.findById(id).get();
+	}
+	public static void deleteById(int id) {
+		bRepo.deleteById(id);
+	}
+}
